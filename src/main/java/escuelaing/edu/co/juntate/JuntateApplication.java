c33714c904d1f037ee34d6c81f979d457304751f
Milton Andres Gutierrez Lopez <80470554+MiltonGutierrez@users.noreply.github.com>
2025-02-03 23:36:57 -0500

    Initial commit

---
+++ src/main/java/escuelaing/edu/co/juntate/JuntateApplication.java
@@ -0,0 +1,13 @@
+package escuelaing.edu.co.juntate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuntateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuntateApplication.class, args);
+	}
+
+}
