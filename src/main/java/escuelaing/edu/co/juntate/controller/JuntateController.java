1fa7bfc76ce4b8ea93a0c833ace451f72c67d3b0
Milton Andres Gutierrez Lopez <80470554+MiltonGutierrez@users.noreply.github.com>
2025-02-25 00:02:11 -0500

    Delete juntate functions

--- src/main/java/escuelaing/edu/co/juntate/controller/JuntateController.java
+++ src/main/java/escuelaing/edu/co/juntate/controller/JuntateController.java
@@ -1,7 +1,6 @@
 package escuelaing.edu.co.juntate.controller;
 
 
-import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,16 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+
 @RestController
 public class JuntateController {
 
 private static final Logger logger = LoggerFactory.getLogger(JuntateController.class);
-
-    @GetMapping
-    public ResponseEntity<List<Event>> getAllEvents() {
-        List<Event> events = juntateService.getAllEvents();
-        return ResponseEntity.ok(events);
-    }
 
     @GetMapping("/health")
     public ResponseEntity<String> health() {
